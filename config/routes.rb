4e1ff0575ec3b0a78eab8cd58c508fc783848f04
zuzuzu <8200514+TaikiSuzuki@users.noreply.github.com>
2025-08-01 19:15:42 +0900

    新規登録、ログインログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'prototypes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+
   root to: "prototypes#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
