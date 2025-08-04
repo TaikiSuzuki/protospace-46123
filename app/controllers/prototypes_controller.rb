d394871740a3e2c9d981551d6d356f41494d92a4
zuzuzu <8200514+TaikiSuzuki@users.noreply.github.com>
2025-08-04 17:51:38 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,10 +9,26 @@ class PrototypesController < ApplicationController
   end
 
   def new
-    @prototype = prototype.new
+    @prototype = Prototype.new
   end
 
+    # Prototype.create(prototype_params)
+    # redirect_to '/'
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save 
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity # 新規投稿ページに戻る
+    end
+  end
+
+
+# private以下の記述はすべてプライベートメソッドになる
+  private
+  
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
 
